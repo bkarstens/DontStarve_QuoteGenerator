38597ce33c7eff3e672424e4dcd3cf81f2d137e0
bkarstens <barrettkarstens@gmail.com>
2016-05-18 22:32:13 -0500

    New Version
    Cleaner, more cases added..just better.

--- QuoteGenerator.py
+++ QuoteGenerator.py
@@ -1,158 +1,511 @@
-SpecialCases = [
-	"HOT",
-	"COLD",
-] # in dictionary as "overheating" and "freezing." Allows to find items like heatrock (hot).
+Overwrite = False#WARNING: ERROR-PRONE
+Debuging = False
+SpecialAffixes = {
+	"HOT":"hot",
+	"COLD":"cold",
+	"PIG": "Pig",
+	"VERYLOW": "very low",
+	"BURNT": "burnt",
+	"INGROUND": "sleeping",
+	"ITEM": "held",
+	"ACTIVE": "active",
+	"STUMP": "stump",
+	"PLANTED": "planted",
+	"WITHERED": "withered",
+	"NOHONEY": "no honey",
+	"FULLHONEY": "full of honey",
+	"SOMEHONEY": "some honey",
+	"DONE": "finished",
+	"COOKING_LONG":"cooking, long time left",
+	"COOKING_SHORT":"cooking, short time left",
+	"NEEDSFERTILIZER": "needs fertilizer",
+	"DRYINGINRAIN": "drying in rain",
+	"WATER": "in water",
+	"LOWFUEL": "low fuel",
+	"VALID": "full",
+	"GEM": "missing gem",
+	"SAND": "sand",
+	"LONG": "stage 1",
+	"MEDIUM": "stage 2",
+	"SOON": "stage 3",
+	"HAUNTED_GROUND": "ready",
+	"HAUNTED_POCKET": "ready, held",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+
+
+
+}
+Cleaning = {
+	"(picked up)": "(held)",
+	"(in the ground)": "(planted)",
+	"(burned out)": "(out)",
+	"(burnt out)": "(out)",
+	"(run out)": "(out)",
+	" (unpicked)":"",
+	"(picked, the remains on the ground)": "(picked)",
+	"(inventory)": "(held)",
+	" (placed)": "",
+	"(turned off)": "(off)",
+	"(turned on)": "(on)",
+	#"Grass Tuft": "Grass", #now conforming to it, not it to me.
+	"Mangrove Tree": "Mangrove",
+	"Octopus Chest-": "Octo Chest-",
+	"Elephant Cactus (withered)": "Elephant Cactus (stump)",#withered = normal, had no spot for stump.
+	"Cooked Wobster": "Delicious Wobster",
+	"Watermelon (cooked)": "Grilled Watermelon",
+	"Carrot (picked)": "Carrot",
+	"Roasted Seeds": "Toasted Seeds",
+	"(summer)": "(empty)",#walruscamp
+	"(full with honey)": "(full of honey)",
+	"Crock Pot-": "Crock Pot (empty)-",
+	"Ash-": "Ashes-",
+	"Divining Rod Base": "Divining Rod Holder",
+	"Wormhole": "Worm Hole",
+	"Bird Egg": "Egg",
+	"Spider Hat": "Spiderhat",
+	"Star-Sky": "Star-sky",
+	"(shaved)": "(naked)",
+	"(following)": "(follower)",
+	"Eyeplant": "Eyeplant",
+	"Lureplant": "Meat Bulb",
+	"Quacken Tentacles": "Quacken Tentacle",
+	"(closed)": "",
+	"Bernie-": "Bernie (active)-",
+	"Water Balloon": "Waterballoon",
+	"Socket (empty)": "Socket (missing gem)",
+	"Focus (incomplete)": "Focus (missing gem)",
+	"Focus (charged)": "Focus (full)",
+	"*Animal Tracks":"*{{Pic32|Shown track}}Animal Track",
+	"(depleted)": "(sand)",
+	"Abigail's flower": "Abigail's Flower",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+
+
+}
+SpecialNounChange = {
+	"Grass-": "Grass Tuft-",
+	"Improved Farm": "Basic and Improved Farm",
+	"Basic Farm": "Basic and Improved Farm",
+	"Net": "Bug Net",
+	"Pig Man": "Pig",
+	"Gem Stand": "Telelocator Socket",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+
+
+}
+SpecialPrefixes = {
+	"DISEASED": "Diseased ",
+	"POISON": "Poison ",
+	"DEAD": "Dead ",#idk if actually doing anything
+	"LIMITED": "Sick ",#1  way worm hole
+
+}
+PicRename = {
+	"A Lumpy Evergreen": "Lumpy Evergreen",
+	"The End is Nigh": "The End is Nigh!",
+	"Sand Bag Structure": "Sandbag",
+	"Sand Bag": "Sandbag Item",
+	"Mangrove Tree": "Mangrove",
+	"Basalt1": "Basalt",
+	"Walrus Camp Winter": "Walrus Camp",
+	"Merm House Shipwrecked": "Merm Hut",
+	"Slotmachine": "Slot Machine",
+	"Octopus Chest Build": "Octo Chest",
+	"Poison Hole": "Poisonous Hole",
+	"X Marks The Spot": "X Marks the Spot",
+	"Mushtree": "Blue Mushtree",
+	"MushtreeRed": "Red Mushtree",
+	"MushtreeGreen": "Green Mushtree",
+	"Monkey Totem": "Splumonkey Pod",
+	"Ancient": "Ancient Statue",
+	"Relic Chair": "Relic",
+	"Rook": "Clockwork Rook",
+	"Damaged bishop": "Damaged Bishop",
+	"Damaged Clockwork": "Damaged Rook",
+	"Ghost Build": "Ghost",
+	"WeeTusk": "Wee MacTusk",
+	"Abigail build": "Abigail",
+	"Cooked Wobster": "Delicious Wobster",
+	"Cooked Coconut": "Roasted Coconut",
+	"Cooked Coffee Beans": "Roasted Coffee Beans",
+	"Webberskull": "Webber's Skull",
+	"Gramophone": "Maxwell's Phonograph",
+	"Minetranspattempt2": "Maxwell's Mosquito Trap",
+	"Rock charcoal": "Charcoal Boulder",
+	"Ash Tree": "Burnt Ash Tree",
+	"Meaty Stew": "Deadly Feast",
+	"Maxwell Head": "Maxwell's Head",
+	"Basic Farm}}{{Pic32|Turbo Farm Plot":"Basic and Improved Farm",#hax
+	"PickSlashAxe": "Pick/Axe",
+	"Berry bush Juicy": "Juicy Berry Bush",
+	"Rabbit Hole": "Crabbit Den",
+	"Ornate Chest": "Large Ornate Chest",
+	"Mermhouse Fisher": "Fishermerm's Hut",
+	"Sinkhole": "Plugged Sinkhole",
+	#"Withered Elephant Cactus": "Elephant Cactus",
+	"Elephant Cactus": "Prickly Elephant Cactus",
+	"Elephant Cactus Stump 2": "Elephant Cactus Stump",
+	"Eyeplant":"Eye Plant",
+	"Lureplant": "Meat Bulb",
+	"Star-Sky": "Star-sky",
+	"Lavae": "Extra-Adorable Lavae",
+	"Grass Tuft": "Grass",
+	"Telelocator_Focus": "Telelocator Socket",
+	"Sea worther": "Sea Worther",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+	#"": "",
+}
+def CreateSkinDict(Source, Character):
+	Source = open(Source)
+	Character = Character.lower()
+	SkinDict = {}
+	for line in Source.readlines():
+		if line.startswith('msgctxt "STRINGS.SKIN_NAMES.body_' + Character + "_"):
+			Entry = line[34+len(Character):-2]
+			Name = Entry.replace("_"," ")
+			Name = Name.title()+" Set"
+			SkinDict[Entry] = Name
+
+	return SkinDict
+def CreateSkinQuoteDict(Source, Character):
+	if Character == "WIGFRID":
+		Character = "WATHGRITHR"
+	SkinDict = CreateSkinDict(Source, Character)
+	Character = Character.lower()
+	Source = open(Source)
+	SkinQuoteDict = {}
+	Entry = None
+	for line in Source.readlines():
+		if Entry:
+			PicName = Character.title()+" "+Entry+" "+"body"
+			#if Debuging:
+				#print("{}_{} PicName: {}".format(Character,Entry, PicName))
+			try:
+				SkinQuoteDict[SkinDict[Entry]] = [line[6:-1], PicName]
+			except:
+				pass
+			Entry = None
+		elif line.startswith('msgctxt "STRINGS.SKIN_QUOTES.{}'.format(Character)):
+			Entry = line[30 + len(Character):-2]
+	try:
+		del SkinQuoteDict[""]
+	except:
+		pass
+	return SkinQuoteDict
 
 def CreateNameDict(Source):
-	f = open(Source)
-	names = {}
-	cname = None
-	for line in f.readlines():
-		if cname:
-			names[cname] = line[7:-2]
-			cname = None
+	Source = open(Source)
+	NameDict = {}
+	Entry = None
+	for line in Source.readlines():
+		if Entry:
+			Entry = Entry.replace("WORMHOLE_LIMITED_1","WORMHOLE_LIMITED")
+			Entry = Entry.replace("SLOW_","")#farms have same quotes
+			Entry = Entry.replace("FAST_","")
+			NameDict[Entry] = line[7:-2]
+			Entry = None
 
 		elif line.startswith('msgctxt "STRINGS.NAMES.'):
-			cname = line[23:-2]
-	return names
+			Entry = line[23:-2]
+		elif line.startswith('msgctxt "STRINGS.DIRECTIONS.'):
+			Entry = line[28:-2]
+		elif line.startswith('msgctxt "STRINGS.SKIN_NAMES.body_'):
+			Entry = line[28:-2]
+	#print(NameDict)
+	return NameDict
 
 def CreateQuoteDict(Source, Character):
-	names = CreateNameDict(Source)
+	if Character == "WILSON": #no Quotes are actually labelled for Wilson, he is the generic.
+		Character = "GENERIC"
+	elif Character == "WIGFRID":
+		Character = "WATHGRITHR"
+	NameDict = CreateNameDict(Source)
 	Character = Character.upper()
-	f = open(Source)
-	quotes = {}
-	caction = None
-	for line in f.readlines():
-		if caction:
+	Source = open(Source)
+	QuoteDict = {}
+	Entry = None
+	for line in Source.readlines():
+		if Entry:
 
 			Prefix = ""
 			Affix = ""
+			Ptemp = ""
+			Atemp = ""
+			Dug = False
+			PicName = ""
+			if Entry[len(Entry)-7:] == "GENERIC":
+				Entry = Entry[:-8]
+				if Debuging: 
+					print("Minus GENERIC:", Entry)
 
-			if caction[len(caction)-7:] == "GENERIC":
-				caction = caction[:-8]
-				#print("Minus GENERIC:", caction)
-			if caction[len(caction)-8:] == "DISEASED":
-					caction = caction[:-9]
-					Prefix = Prefix + "Diseased "
-					#print("Minus DISEASED:", caction)
-			if caction.startswith("DESCRIBE"):
-				caction = caction[9:]
-				#print("Minus DESCRIBE:", caction)
+			if Entry.startswith("DESCRIBE"):
+				Entry = Entry[9:]
+				if Debuging: 
+					print("Minus DESCRIBE:", Entry)
 
-			if caction.startswith("ANNOUNCE"):
-				caction = caction[9:]
-				#print("Minus ANNOUNCE:", caction)
+			elif Entry.startswith("ANNOUNCE"):
+				Entry = Entry[9:]
+				if Debuging: 
+					print("Minus ANNOUNCE:", Entry)
 
-			if caction.find(".") != -1:
-				i = caction.find(".")
-				Affix = caction[i+1:]#-----no "."
-				if len(Affix) > 2 and Affix not in SpecialCases:#--------------min length 3 so compass directions stay caps
+			if Entry.startswith("DUG"):
+				Entry = Entry[4:]
+				Affix = "held"
+				Dug = True#-----distinction from non-Dug verison
+				if Debuging: 
+					print("Minus DUG:", Entry)
+			for Case in SpecialPrefixes:
+				if Entry[len(Entry) - (len(Case) + 1):] == "." + Case or Entry[len(Entry) - (len(Case) + 1):] == "_" + Case:
+					Entry = Entry[:len(Case) + 1]
+					Ptemp = Case
+					Prefix = Prefix + SpecialPrefixes[Case]
+					if Debuging: 
+						print("SpecialPrefixes: {}=>{}".format(Case,SpecialPrefixes[Case]))
+					break
+
+			for Case in SpecialAffixes:
+				if Entry[len(Entry) - (len(Case) + 1):] == "." + Case or Entry[len(Entry) - (len(Case) + 1):] == "_" + Case:
+					Entry = Entry[:len(Entry) - len(Case)-1]
+					Atemp = Case
+					Affix = SpecialAffixes[Case]
+					if Debuging: 
+						print("SpecialAffixes: {}=>{}".format(Case,SpecialAffixes[Case]))
+					
+					break
+
+			if Entry.find(".") != -1:#-----NORMAL AFFIX GENERATOR
+				i = Entry.find(".")
+				Affix = Entry[i+1:]#-----also removes "."
+				if Affix not in SpecialAffixes:
 					try:
-						Affix = names[Affix]
+						Affix = NameDict[Affix]
 					except KeyError:
-						if Affix == "VERYLOW":
-								Affix = Affix[:4] + " " + Affix[4:]
 						Affix = Affix.lower()
-				if Affix == "PIG":
-					Affix = Affix.capitalize()
-				else:
-					Affix = Affix.lower()		
+				Entry = Entry[:i]
+			if Affix:
 				Affix = " ({})".format(Affix)
-				#print("Affix:", Affix)
-				caction = caction[:i]
-				#print("Minus Affix:", caction)
+				if Debuging: 
+					print("Affix:", Affix)
+					print("Minus Affix:", Entry)
+
+			for Case in SpecialNounChange:
+				try:
+					if NameDict[Entry] == Case:
+						NameDict[Entry] = SpecialNounChange[Case]
+						break
+				except KeyError:
+					pass
+			
+
+
+			if Dug and "DUG_" + Entry in NameDict:
+				if Debuging: 
+					print("DUG_" + Entry)
+				PicName = NameDict["DUG_" + Entry]
+			if Entry + "_" + Ptemp in NameDict:#Ptemp and Atemp are what the prefixes/affixes were before going through the Special________ functions
+				if Debuging: 
+					print(Entry + "_" + Ptemp)
+				PicName = NameDict[Entry + "_" + Ptemp]
+			elif Entry + "." + Ptemp in NameDict:
+				if Debuging: 
+					print(Entry + "." + Ptemp)
+				PicName = NameDict[Entry + "." + Ptemp]
+
+			elif Entry + "_" + Atemp in NameDict:
+				if Debuging: 
+					print (Entry + "_" + Atemp)
+				PicName = NameDict[Entry + "_" + Atemp]
+
+			elif Entry + "." + Atemp in NameDict:
+				if Debuging: 
+					print (Entry + "." + Atemp)
+				PicName = NameDict[Entry + "." + Atemp]
+
+			elif Entry in NameDict:
+				PicName = NameDict[Entry]
+			if PicName:
+				if Debuging: 
+					print("PicName: ", PicName)
+				pass
+
+
 			try:
-					#print("IGN:", names[caction])
-					quotes[Prefix + names[caction]+ Affix] = line[6:-1]
-					#print("––––––––––––––––––––––––––––––––––––––––{}- {}".format(Prefix + names[caction] + Affix, quotes[Prefix + names[caction] + Affix]))
+				if Debuging: 
+					print("Entry:", Entry)
+				if Entry == "COMPASS":
+					pass
+				else:
+					QuoteDict[Prefix + NameDict[Entry]+ Affix] = [line[6:-1], PicName]
+					if Debuging: 
+						print("IGN:", NameDict[Entry])
+						print("––––––––––––––––––––––––––––––––––––––––{}- {}".format(Prefix + NameDict[Entry] + Affix, QuoteDict[Prefix + NameDict[Entry] + Affix][0]))
 			except KeyError:
-				#print("====Not in Dictionary====")
-				caction = caction.replace("_"," ")
-				caction = caction.replace("."," ")
-				#caction = caction.replace("\"","")
-				caction = caction.capitalize()
-				#print("Cleaned Action:", caction)
-				quotes[Prefix + caction + Affix] = line[6:-1]
-				#print("––––––––––––––––––––––––––––––––––––––––{}- {}".format(Prefix + caction + Affix, quotes[Prefix + caction + Affix]))
-			caction = None
+				if Debuging: 
+					print("====Not in Dictionary====")
+				Entry = Entry.replace("_"," ")
+				Entry = Entry.replace("."," ")
+				Entry = Entry.replace("\"","'")
+				Entry = Entry.title()
+				if Debuging: 
+					print("Cleaned Action:", Prefix + Entry + Affix)
+				QuoteDict[Prefix + Entry + Affix] = [line[6:-1], PicName]
+				if Debuging: 
+					print("––––––––––––––––––––––––––––––––––––––––{}- {}".format(Prefix + Entry + Affix, QuoteDict[Prefix + Entry + Affix][0]))
+			Entry = None
 
 		elif line.startswith('msgctxt "STRINGS.CHARACTERS.{}'.format(Character)):
-			caction = line[29 + len(Character):-2]
-			#print ("Raw action:",caction)
-	return quotes
+			Entry = line[29 + len(Character):-2]
+			if Debuging: 
+				print ("Raw action:",Entry)
+	try:
+		del QuoteDict[""]
+	except:
+		pass
+	#print(QuoteDict)
+	return QuoteDict
 
 def EnterQuotes(Source, Character, InputFile, OutputFile):
 	print("========================================")
 	print("Using Source: {}".format(Source))
 	Character = Character.upper()
-	if Character == "WILSON": #no quotes are actually labelled for Wilson, he is the generic.
-		Character = "GENERIC"
-	QuoteDict = CreateQuoteDict(Source, Character)
+	if Character == "MAXWELL":
+		Character = "WAXWELL"
+	QuoteDict = dict(list(CreateQuoteDict(Source, Character).items()) + list(CreateSkinQuoteDict(Source, Character).items()))
 	Used = []
-	f = open(InputFile)
-	Appendedf = open(OutputFile, "w")
-	print("Quotes added:")
-	for line in f.readlines():
-
+	Input = open(InputFile)
+	Output = open(OutputFile, "w")
+	lIndex = 0
+	
+	print("Creating file", end="",flush=True)
+	for line in Input.readlines():
+		lIndex += 1
+		for Case in Cleaning:
+			line = line.replace(Case, Cleaning[Case])
+		
 		for key in QuoteDict:
+			temp = QuoteDict[key][0]
+			if Character == "WATHGRITHR" or Character == "WIGFRID":
+				temp = temp.replace("o", "ö")
+				temp = temp.replace("O", "Ö")
 
-			if line.find("*{}- {}".format(key, QuoteDict[key])) != -1:
-				if key not in Used:
-					Used.append(key)
+			PicName = QuoteDict[key][1]
+			Icon = "{}{}{}".format("{{Pic32|", PicName, "}}")
+			#if Debuging: 
+			#	print ("Name: {}|Quote: {}|PicName: {}".format(key, temp, QuoteDict[key][1]))
+			if Overwrite:
+				if line.find("*{}-".format(key)) != -1 or line.find("] {}-".format(key)) != -1 or line.find("]{}-".format(key)) != -1 or line.find("{} {}-".format("}", key)) != -1 or line.find("{}{}-".format("}", key)) != -1:
+				
+					if PicName:
+						line = "*" + Icon + "{}- {}\n".format(key, temp)
+					else:
+						line ="*{}- {}\n".format(key, temp)
+					break
+
+			elif line.find("] {}-\n".format(key)) != -1 or line.find("]{}-\n".format(key)) != -1 or line.find("] {}- \n".format(key)) != -1 or line.find("]{}- \n".format(key)) != -1 or \
+			line.find("{} {}-\n".format("}", key)) != -1 or line.find("{}{}-\n".format("}", key)) != -1 or line.find("{} {}- \n".format("}", key)) != -1 or line.find("{}{}- \n".format("}", key)) != -1 or \
+			line.find("*{}-\n".format(key)) != -1 or line.find("*{}- \n".format(key)) != -1:#cant use line.find("{}- \n".format(key)) b/c 'Endothermic Fire' would also trigger 'Fire'
+
+				if PicName:
+					line = "*" + Icon + "{}- {}\n".format(key, temp)
+				else:
+					line ="*{}- {}\n".format(key, temp)
 				break
 
-			if line.find("*{}-\n".format(key)) != -1: #----------if spot doesnt have " "
-				temp = QuoteDict[key]
-				if Character == "WATHGRITHR":
-					temp = temp.replace("o", "ö")
-					temp = temp.replace("O", "Ö")
-				index = line.find("*{}-\n".format(key))
-				line = line[:index + len(key)+2] + " " + temp + line[index + len(key)+2:]
-				print (line)
-				if key not in Used:
-					Used.append(key)
-				index = None
-				break
+		for Case in PicRename:
+			line = line.replace("|" + PicRename[Case] + "}}", "|" + Case + "}}")
+		Output.write(line)
 
-			elif line.find("*{}- \n".format(key)) != -1: #----------if spot has " "
-				temp = QuoteDict[key]
-				if Character == "WATHGRITHR":
-					temp = temp.replace("o", "ö")
-					temp = temp.replace("O", "Ö")
-				index = line.find("*{}- \n".format(key))
-				line = line[:index + len(key)+3] + temp + line[index + len(key)+3:]
-				print (line)
-				if key not in Used:
-					Used.append(key)
-				index = None
-				break
-
-
-		Appendedf.write(line)
+		if lIndex%100 == 0:
+			print('.', end="",flush=True)
 	else:
-		print("––––––––––––––––––––––––––––––––––––––––")
-		"""if Used != "": #----------shows actions found in source but couldn't find/place in file. The file here isn't the prettiest.
-			Appendedm = open("unused_" + OutputFile, "w")
-			print("Quotes not used in {}".format("unused_" + OutputFile))
-			for key in Used:
-				del QuoteDict[key]
-			for key in QuoteDict:
-				Appendedm.write("*{}- {}\n".format(key, QuoteDict[key]))"""
+		"""Unused = open("unused_" + Character.lower() + ".txt", "a")
+		Unused.write("––––––––––––––––––––––––––––––––––––––––\n")
+		UnusedR = open("unused_" + Character.lower() + ".txt", "r")
+								
+		for key in QuoteDict:
+			if Input.read().find("*{}- {}".format(key, QuoteDict[key])) == -1:
+				if UnusedR.read().find("*{}- {}\n".format(key, QuoteDict[key])) == -1:
+					Unused.write("*{}- {}\n".format(key, QuoteDict[key]))"""
+		print("Complete")
+
+
+		if Debuging: 
+			print("––––––––––––––––––––––––––––––––––––––––")
 		print('Exported to "{}"'.format(OutputFile))
-		print("Done!")
-def GetQuote(Source, Character, entity):
-	Dict = CreateQuoteDict(Source, Character)
-	try:
-		return Dict[entity]
-	except:
-		pass
 
 
 
 
 
 
-Character = input("Caracter DebugSpawn (file should also be named Character.txt):")
+
+
+Character = input("Character (file should also be named Character.txt):")
 #EnterQuotes("strings.pot",input("Character DebugSpawn: "), input("Import file (e.x. file.txt) :"), input("Export file (e.x. file.txt) :"))
-EnterQuotes("strings.pot",Character, Character + ".txt", Character + "temp.txt")
-EnterQuotes("stringsDST.pot",Character, Character + "temp.txt", Character + "_Export.txt")
+EnterQuotes("stringsDST.pot",Character, Character + ".txt", Character + "temp.txt")
+EnterQuotes("strings.pot",Character, Character + "temp.txt", Character + "_Export.txt")
+print("========================================")
+print("Done!")
